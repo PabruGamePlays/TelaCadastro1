6a2922328842f73b98f92b56a9b78dc8c535d655
Pablo Miguel <103229513+PabruGamePlays@users.noreply.github.com>
2022-04-28 10:30:56 -0300

    Create CadastroPessoaAC1.java
    Classe Main que faz o projeto iniciar

---
+++ CadastroPessoaAC1.java
@@ -0,0 +1,15 @@
+package ClassePrincipal;
+
+import Classe_Pessoas.Pessoa;
+import Telas.MenuTela;
+
+
+public class CadastroPessoasAC1 {
+    public static void main(String[] args) {
+       
+        MenuTela Menu = new MenuTela();
+        Menu.setVisible(true);
+        
+    }
+    
+}
