8bc5e13e0a25f80aa5935ed8579cec51ccf60048
Pablo Miguel <103229513+PabruGamePlays@users.noreply.github.com>
2022-04-28 10:40:26 -0300

    Create PessoaFisica.java
    Objeto pessoa física que implementa com o objeto Pessoa

---
+++ PessoaFisica.java
@@ -0,0 +1,54 @@
+package Classe_Pessoas;
+public class PessoaFisica extends Pessoa{
+
+    private String RG;
+    private String CPF;
+
+    public PessoaFisica(){
+    
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public void setNome(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setEndereco(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setBairro(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setCep(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setCidade(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setEstado(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+       
+        
+}
