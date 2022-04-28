101e68fe27326e7c577f98ba1c80de6e6d1e58bf
Pablo Miguel <103229513+PabruGamePlays@users.noreply.github.com>
2022-04-28 11:41:43 -0300

    Create Contato.java
    Classe que atribui o contato e é implementado as classes de pessoas

---
+++ Contato.java
@@ -0,0 +1,36 @@
+package cadastropessoasac1;
+public class Contato {
+    
+    private String email;
+    private String ddd;
+    private String telefone;
+    
+    public Contato(){
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+}
